7dc160af7eefa31fac4f3a62d74b358d8d584c6a
Chase-Edwards <chase.b.edwards1@gmail.com>
2024-04-20 22:54:30 +0100

    Cleaning up repo, moved common functions to new SpotifyAgent class. Updated directory structure.
    At this point, app runs successfully, creates
    playlist and adds 2 tracks to it.

---
+++ festival-spotify-playlist/helpers.py
@@ -0,0 +1,92 @@
+import os
+import spotipy
+
+class SpotifyAgent:
+    def __init__(self):
+        """
+        Initialize the SpotifyAgent class.
+
+        Args:
+            client_id (str): The client ID for the Spotify API.
+            client_secret (str): The client secret for the Spotify API.
+        """
+        self.client_id = os.getenv("SPOTIPY_CLIENT_ID")
+        self.client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+        self.redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+        self.scope = "playlist-modify-public"
+        self.playlist_id = None
+        self.oauth_manager = spotipy.SpotifyOAuth(client_id=self.client_id,
+                                                  client_secret=self.client_secret,
+                                                  redirect_uri=self.redirect_uri,
+                                                  scope=self.scope)
+        self.client = spotipy.Spotify(auth_manager=self.oauth_manager)
+        self.current_user_id = self.client.current_user()['id']
+
+
+    def get_artist_id(self, artist_name):
+        """
+        Get the Spotify ID of an artist.
+
+        Args:
+            client (Spotify): The Spotify client object.
+            artist_name (str): The artist name.
+
+        Returns:
+            str: The Spotify ID of the artist.
+        """
+        search_query = 'q=' + 'artist%3A' + artist_name
+        response = self.client.search(search_query, limit=1, type='artist')
+        artist_id = response['artists']['items'][0]['id']
+
+        return artist_id
+
+
+    def create_playlist(self, name, description):
+        """
+        Create a public playlist for the current user.
+
+        Args:
+            name (str): The new playlist name.
+            description (str): The new playlist description.
+        """
+        playlist = self.client.user_playlist_create(user=self.current_user_id, name=name, description=description)
+        self.playlist_id = playlist['id']
+
+
+    def get_tracks(self, artist_id, amount):
+        """
+        Gets a given amount of tracks from an artist.
+
+        Args:
+            artist_id (str): The ID of the artist whose songs will be searched for.
+            amount    (int): The number of songs to add.
+
+        Returns:
+            tracks (dict): A dictionary of tracks.
+        """
+        if amount > 10:
+            raise ValueError("amount must be 10 or less.")
+        
+        return self.client.artist_top_tracks(artist_id)['tracks'][:amount]
+
+
+    def add_tracks(self, tracks):
+        """
+        Add an amount of songs to a playlist with the given playlist ID.
+
+        Args:
+            playlist_id (str): The ID of the playlist to be deleted.
+            tracks (dict): A dictionary of tracks.
+        """
+        self.client.playlist_add_items(self.playlist_id, [track['id'] for track in tracks])
+        
+
+    def delete_playlist(self):
+        """
+        Delete a playlist with the given playlist ID.
+        """
+        if self.playlist_id:
+            self.client.playlist_unfollow(self.playlist_id)
+        
+        else:
+            print("No playlist id set.")
---
+++ festival-spotify-playlist/main.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+import helpers as h
+
+
+def main():
+    
+    
+    load_dotenv('.\\secrets.env', override=True)
+
+    # TODO: ADD READ OF LINEUP FILE + COMPREHENSION
+    artist_name = "EPROM"
+    playlist_name='Rampage Open Air 2024 - Discord'
+    playlist_description='A playlist for the Rampage Open Air 2024 festival lineup'
+        
+    # Create a Spotify agent object
+    agent = h.SpotifyAgent()
+
+    # Get the Spotify ID of the artist
+    artist_id = agent.get_artist_id(artist_name)
+
+    tracks = agent.get_tracks(artist_id, 2)
+
+    playlist_id = agent.create_playlist(playlist_name, playlist_description)
+
+    # Add the tracks to the playlist
+    agent.add_tracks(tracks)
+
+    pass
+
+if __name__ == "__main__":
+    main()
