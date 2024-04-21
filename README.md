bcf325d0b918fc8edf35b3c71d67a7657ef6077e
Chase-Edwards <chase.b.edwards1@gmail.com>
2024-04-21 01:43:39 +0100

    Added data on lineup as JSON asset and README  update

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # festival-spotify-playlist
 This application generates a spotify playlist from a list of artists. The intended use is for festival lineups.
 
+# Environment Setup
+
+Create a `.env` file and place it in the top level workspace folder. Add the following three ENV variables:
+
+1) SPOTIPY_CLIENT_ID
+2) SPOTIPY_CLIENT_SECRET
+3) SPOTIPY_REDIRECT_URI
+
+In `main`, when `load_dotenv` is called, it requires a path. Replace `.\\secrets.env` with the name of the .env file you created. 
 
 # Note on playlist creation
 The playlist created will be public and non-collaborative to start, as it will be used with the discord bot.
--- festival-spotify-playlist/main.py
+++ festival-spotify-playlist/main.py
@@ -1,22 +1,27 @@
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 import spotify_agent as agent
+import json
 
 
 def main():
-    
-    
+    # Load environment variables
     load_dotenv('.\\secrets.env', override=True)
-
-    # TODO: ADD READ OF LINEUP FILE + COMPREHENSION
-    artist_name = "EPROM"
+    
     playlist_name='Rampage Open Air 2024 - Discord'
     playlist_description='A playlist for the Rampage Open Air 2024 festival lineup'
         
     # Create a Spotify agent object
     agent = agent.SpotifyAgent()
 
+    artists = json.loads('.\\data\\lineup.json')
+
+    for artist in artists:
+        #TODO: Implement looped calls
+        pass
+
     # Get the Spotify ID of the artist
+    
     artist_id = agent.get_artist_id(artist_name)
 
     tracks = agent.get_tracks(artist_id, 2)
